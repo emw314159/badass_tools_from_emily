643504b9e6d7496ac9f86a189568c5d6bd208a6c
Emily Williams <emw314159@gmail.com>
2017-07-05 05:54:59 -0700

    Added features such as row coloring.

--- examples/test_html.py
+++ examples/test_html.py
@@ -6,9 +6,10 @@ import numpy as np
 
 
 def main():
-    df = pd.read_csv('df_first_WITH_SCORES.csv')
+    df = pd.read_csv('data/dataframe.csv')
     f = open('output/test_df.html', 'w')
-    ht.start_HTML(f, 'Emily\'s HTML Automation', 'DataFrame to HTML Example:')
+    ht.start_HTML(f, 'Emily\'s HTML Automation Examples')
+
     headers = ['sequence(target+3\'+5\')', 'log2 fold change, KBM7', 'seed', 'motif_scores', 'motif_scores_seed', 'regression_score']
 
     style = {
@@ -20,30 +21,34 @@ def main():
         'regression_score' : ('float', 3),
     }
 
+    f.write('<h1>Dataframe to Table Example:</h1>')
+    f.write(ht.dataframe_to_table_HTML(df.iloc[range(0, 20),:], 'emily-df', headers=headers, style=style))
 
-    #formula = 'result_KBM7 ~ folding_dG + motif_scores + motif_scores_seed + be_seed_5 + be_seed_6 + se_full + se_seed_5'
-    #model = smf.ols(formula=formula, data=df).fit()
-    #print dir(model)
-    #print model.cov_params()
-    #print model.summary()
+#     #formula = 'result_KBM7 ~ folding_dG + motif_scores + motif_scores_seed + be_seed_5 + be_seed_6 + se_full + se_seed_5'
+#     #model = smf.ols(formula=formula, data=df).fit()
+#     #print dir(model)
+#     #print model.cov_params()
+#     #print model.summary()
      
-    df2 = pd.DataFrame()
-    df2['Intercept'] = len(list(df['result_KBM7'])) * [1]
-    df2['folding_dG'] = list(df['folding_dG'])
-    df2['motif_scores'] = list(df['motif_scores'])
-    df2['motif_scores_seed'] = list(df['motif_scores_seed'])
-    df2['be_seed_5'] = list(df['be_seed_5'])
-    df2['be_seed_6'] = list(df['be_seed_6'])
-    df2['se_seed_5'] = list(df['se_seed_5'])
-    df2['se_full'] = list(df['se_full'])
-    df2m = df2.as_matrix()
-    corr_matrix = np.corrcoef(df2m, rowvar=0)
-    corr_html = ht.correlation_matrix_HTML(corr_matrix, list(df2.columns.values))
+#     df2 = pd.DataFrame()
+#     df2['Intercept'] = len(list(df['result_KBM7'])) * [1]
+#     df2['folding_dG'] = list(df['folding_dG'])
+#     df2['motif_scores'] = list(df['motif_scores'])
+#     df2['motif_scores_seed'] = list(df['motif_scores_seed'])
+#     df2['be_seed_5'] = list(df['be_seed_5'])
+#     df2['be_seed_6'] = list(df['be_seed_6'])
+#     df2['se_seed_5'] = list(df['se_seed_5'])
+#     df2['se_full'] = list(df['se_full'])
+#     df2m = df2.as_matrix()
+#     corr_matrix = np.corrcoef(df2m, rowvar=0)
+#     corr_html = ht.correlation_matrix_HTML(corr_matrix, list(df2.columns.values))
     
 
    
 
-    f.write(ht.dataframe_to_table_HTML(df.iloc[range(0, 20),:], 'emily-df', headers=headers, style=style))
+
+
+
     ht.end_and_close_HTML(f)
 
 
