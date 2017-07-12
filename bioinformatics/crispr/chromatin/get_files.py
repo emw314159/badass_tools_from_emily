9b467f858a867eb28a2bb1e8865f7bba7eb47b9e
Emily.Williams@thermofisher.com <Emily.Williams@thermofisher.com>
2017-07-11 21:54:57 -0700

    Added steps to process metadata.

--- bioinformatics/crispr/chromatin/get_files.py
+++ bioinformatics/crispr/chromatin/get_files.py
@@ -12,6 +12,8 @@ import pickle
 #
 os.system('mkdir downloaded_files')
 os.system('mkdir output')
+os.system('mkdir downloaded_files/combined')
+os.system('mkdir downloaded_files/combined_merged')
 
 #
 # download the files
@@ -29,10 +31,81 @@ f.close()
 os.system('gunzip downloaded_files/*.gz')
 
 #
+# metadata
+# 
+bio = {}
+f = open('downloaded_files/metadata.tsv')
+for line in f:
+    line = [x.strip() for x in line.split('\t')]
+    if line[0] == 'File accession':
+        continue
+    filename = 'downloaded_files/' + line[0] + '.bed'
+    biosample = line[6].replace(' ', '_').replace('/', '-')
+    if not bio.has_key(biosample):
+        bio[biosample] = []
+    bio[biosample].append(filename)
+f.close()
+
+
+
+#
+# join files
+#
+print 'Joining files...'
+for biosample in sorted(bio.keys()):
+    filelist = bio[biosample]
+    newfile = 'downloaded_files/combined/' + biosample + '.bed'
+    cmd = 'cat ' + ' '.join(filelist) + ' > ' + newfile
+    os.system(cmd)
+
+#
+# sort
+#
+print 'Sorting files...'
+filelist = glob.glob('downloaded_files/combined/*.bed')
+for filename in filelist:
+    coords = {}
+    f = open(filename)
+    for line in f:
+        line = [x.strip() for x in line.split('\t')]
+        chrom = line[0]
+        try:
+            start = int(line[1])
+            end = int(line[2])
+        except:
+            continue
+        if not coords.has_key(chrom):
+            coords[chrom] = {}
+        if not coords[chrom].has_key(start):
+            coords[chrom][start] = {}
+        coords[chrom][start][end] = None
+    f.close()
+    
+    newfile = filename.replace('.bed', '') + '__SORTED.bed'
+    f = open(newfile, 'w')
+    for chrom in sorted(coords.keys()):
+        for start in sorted(coords[chrom].keys()):
+            for end in sorted(coords[chrom][start].keys()):
+                f.write('\t'.join([chrom, str(start), str(end)]) + '\n')
+    f.close()
+
+#
+# merge
+#
+print 'Merging files...'
+filelist = glob.glob('downloaded_files/combined/*__SORTED.bed')
+for filename in filelist:
+    short_name = filename.split('/')[-1].replace('__SORTED.bed', '.bed')
+    cmd = 'bedtools merge -i ' + filename + ' > downloaded_files/combined_merged/' + short_name
+    os.system(cmd)
+
+
+#
 # process files
 #
+print 'Building counts data structure...'
 counts = {}
-filelist = glob.glob('downloaded_files/*.bed')
+filelist = glob.glob('downloaded_files/combined_merged/*.bed')
 for filename in filelist:
     f = open(filename)
     for line in f:
@@ -54,5 +127,6 @@ for filename in filelist:
 #
 # save counts
 #
+print 'Saving results...'
 with open('output/counts.pickled', 'w') as f:
     pickle.dump(counts, f)
