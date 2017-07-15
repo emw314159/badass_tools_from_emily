adedae8641611f17985d37b692c8a21122a5aa4e
Emily Williams <emw314159@gmail.com>
2017-07-15 13:24:49 -0700

    It works now.

--- web_apps/django_examples/fashionistas_astronomy_calculations/static/fac_index.js
+++ web_apps/django_examples/fashionistas_astronomy_calculations/static/fac_index.js
@@ -1,19 +1,44 @@
-var app = angular.module('fashionistas-solar-app', []);
+var app = angular.module('fashionistas_solar_app', []);
 
-app.controller('fashionistas-solar-controller', function($scope) {
+app.controller('fashionistas_solar_controller', ['$scope', '$http', function($scope, $http) {
 	
 	/* 
 	   Find the user's time zone
-
 	 */
-	var tz = 'GMT' + Date().toString().split('GMT')[1].split(' ')[0]
+	var timezone = 'GMT' + Date().toString().split('GMT')[1].split(' ')[0]
 
 	/*
 	  Initialize variables
 	*/
-	$scope.tz = tz
+	$scope.timezone = timezone
 	$scope.latitude = 33.1192;
 	$scope.longitude = -117.0864;
+	$scope.have_result = false;
+	var url = 'http://localhost:8000/a-fashionistas-astronomy-calculations/logic';
 
+	/*
+	  Process data
+	*/
+	$scope.buttonClicked = function() {
+	    $scope.have_result = false;
+	    if ($scope.fashionistas_form.$valid) {
+		var data_to_send = {}
+		data_to_send['timezone'] = $scope.timezone;
+		data_to_send['latitude'] = $scope.latitude;
+		data_to_send['longitude'] = $scope.longitude;
+		$http({
+			method : 'POST',
+			    data : data_to_send,
+			    url : url,
+			    }).then(function successCallback(response) {
+				    console.log(response);
+				    $scope.utc_noon = response['data']['utc_noon'];
+				    $scope.local_noon = response['data']['local_noon'];
+				    $scope.have_result = true;
+				}, function errorCallback(response) {
+				});
+	    }
 
-    });
+	};
+
+    }]);
--- web_apps/django_examples/fashionistas_astronomy_calculations/views.py
+++ web_apps/django_examples/fashionistas_astronomy_calculations/views.py
@@ -2,11 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-#from django.http import JsonResponse
+from django.http import JsonResponse
 from django.http import HttpResponse
 from django.template import loader
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
+#from rest_framework.response import Response
 
 import pytz
 from datetime import datetime
@@ -19,14 +19,14 @@ def index(request):
     template = loader.get_template('fac_index.html')
     return HttpResponse(template.render({}, request))
 
-#
-# view to return JSON containing a list of all time zones
-#
-@api_view(['GET', 'POST'])
-def get_all_timezones(request):
-    data_to_return = {'all_timezones' : pytz.all_timezones}
-    #return JsonResponse(data_to_return)
-    return Response(data_to_return)
+# #
+# # view to return JSON containing a list of all time zones
+# #
+# @api_view(['GET', 'POST'])
+# def get_all_timezones(request):
+#     data_to_return = {'all_timezones' : pytz.all_timezones}
+#     #return JsonResponse(data_to_return)
+#     return Response(data_to_return)
 
 #
 # view to return solar noon
@@ -35,8 +35,32 @@ def get_all_timezones(request):
 def solar_noon(request):
     data_to_return = {}
     if request.method == 'POST':
-        pass
-    return Response(data_to_return)
+
+        #
+        # calculate solar noon in UTC
+        #
+        o = ephem.Observer()
+        o.lat, o.long = str(request.data['latitude']), str(request.data['longitude'])
+        sun = ephem.Sun()
+        sunrise = o.previous_rising(sun, start=ephem.now())
+        noon = o.next_transit(sun, start=sunrise)
+
+        #
+        # convert to timezone
+        #
+        user_timezone = request.data['timezone']
+        hours_to_offset = float(user_timezone.replace('GMT', '')) / 100.
+        days_to_offset = hours_to_offset / 24.
+        noon = float(noon)
+        local_noon = noon + days_to_offset
+
+        #
+        # set data to return
+        #
+        data_to_return['utc_noon'] = str(ephem.Date(noon)).split(' ')[1]
+        data_to_return['local_noon'] = str(ephem.Date(local_noon)).split(' ')[1]
+
+    return JsonResponse(data_to_return)
 
 
     
