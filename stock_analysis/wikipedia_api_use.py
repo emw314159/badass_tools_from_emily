a21d2e53e15540906922c6048cc0d40c2162d285
Emily Williams <emw314159@gmail.com>
2017-07-24 13:45:41 -0700

    Got it running to some degree.

--- stock_analysis/wikipedia_api_use.py
+++ stock_analysis/wikipedia_api_use.py
@@ -1,88 +1,97 @@
 
+#
+# import useful libraries
+#
 import requests
 import pprint as pp
+import json
+
+#
+# user settings
+#
+words = ['ceo', 'cfo', 'cto', 'cdo', 'chairman', 'chairwoman', 'chair', 'president', 'chief executive officer', 'and', 'businessman', 'businesswoman', 'executive', 'num_employees', 'employees', 'apple', '{{small', '*', 'chief design officer', 'chief operating officer', 'product', 'products', '{{plainlist', 'ref name', 'coo', 'chief financial officer', 'area_served', '{{startplainlist\n*', '{{startplainlist']
+
+url_part_1 = 'https://en.wikipedia.org/w/api.php?action=query&titles='
+url_part_2 = '&format=json&prop=revisions&rvprop=content'
+
+output_directory = 'output'
 
 
-words = ['ceo', 'cfo', 'cto', 'cdo', 'chairman', 'chairwoman', 'chair', 'president', 'chief executive officer', 'and', 'businessman', 'businesswoman', 'executive']
 
+#
+# load names
+#
+with open(output_directory + '/symbol_to_name.json') as f:
+    symbol_to_name = json.load(f)
 
-
-symbols = {
-    'ANET' : 'https://en.wikipedia.org/w/api.php?action=query&titles=Arista%20Networks&format=json&prop=revisions&rvprop=content',
-    'F' : 'https://en.wikipedia.org/w/api.php?action=query&titles=Ford%20Motor%20Company&format=json&prop=revisions&rvprop=content',
-}
 
 people_dict = {}
 
-for symbol in symbols:
+for i, symbol in enumerate(sorted(symbol_to_name.keys())):
+    
+    print i
 
-    url = symbols[symbol]
-    people_dict[symbol] = {}
+    try:
 
-    r = requests.get(url)
+        name = symbol_to_name[symbol].keys()[0]
 
-    page = r.json()['query']['pages'].keys()[0]
-    data = r.json()['query']['pages'][page]
+        name = name.replace(', Inc.', '')
+        name = name.replace(' Corporation', '')
+        name = name.replace(' Limited', '')
+
+        url = url_part_1 + name.replace(' ', '%20') + url_part_2
 
 
-    #document = data['revisions'][0]['*'].split('|')
-    #document = [x.replace('[', '').replace(']', '') for x in document if x.find('key_people') >= 0]
+        people_dict[symbol] = {}
+        
+        r = requests.get(url)
+        
+        page = r.json()['query']['pages'].keys()[0]
+        data = r.json()['query']['pages'][page]
 
-    document = data['revisions'][0]['*'].split('key_people')[1].split('=')[1]
-    document = document.replace('industry', '')
-    document = document.replace('{{unbulleted list', '').replace('}}', '')
-    document = document.replace('<small>', '').replace('</small', '')
 
-    document = document.replace('<br/>', '|').replace('<br />', '|')
-    document = document.replace('>', '').replace('<', '')
-    document = document.replace('(', '').replace(')', '')
-    document = document.replace('[[', '|').replace(']]', '|')
-    document = document.replace('&', '|')
 
-    document = [x.strip() for x in document.split('|') if x.strip() != '']
+
+        document = data['revisions'][0]['*'].split('key_people')[1].split('=')[1]
+        document = document.replace('industry', '')
+        document = document.replace('{{unbulleted list', '').replace('}}', '')
+        document = document.replace('<small>', '').replace('</small', '')
+        
+        document = document.replace('<br/>', '|').replace('<br />', '|')
+        document = document.replace('>', '').replace('<', '')
+        document = document.replace('(', '').replace(')', '')
+        document = document.replace('[[', '|').replace(']]', '|')
+        document = document.replace('&', '|')
+
+        document = [x.strip() for x in document.split('|') if x.strip() != '']
     
 
-    new_document = []
-    for d in document:
-        word_to_test = d.lower().replace(',', '').strip()
-        if not word_to_test in words:
-            new_short_list = []
-            short_list = word_to_test.split(' ')
-            for s in short_list:
-                if not s.lower().replace(',', '').strip() in words:
-                    new_short_list.append(s.strip())
+        new_document = {}
+        for d in document:
+            word_to_test = d.lower().replace(',', '').strip()
+            if not word_to_test in words:
+                new_short_list = []
+                short_list = word_to_test.split(' ')
+                for s in short_list:
+                    if not s.lower().replace(',', '').strip() in words:
+                        if s.lower().find('employee') == -1:
+                            new_short_list.append(s.strip())
 
-            if len(new_short_list) != 0:
-                new_document.append(' '.join(new_short_list))
+                if len(new_short_list) != 0:
+                    new_document[' '.join(new_short_list)] = None
 
+        people_dict[symbol] = new_document
 
-
-
-    print
-    print new_document
-    print
-
-    continue
+    except:
+        continue
 
 
+#
+# save first pass of the dictionary
+#
+with open(output_directory + '/people_first_pass.json', 'w') as f:
+    json.dump(people_dict, f)
 
-    for d in document:
-        people = [x.replace('>', '') for x in d.split('=')[1].strip().split('<br/')]
-        for i, p in enumerate(people):
-            if p[-1] == ',':
-                people[i] = p[0:-1]
-            people_and_position = []
-            for p in people:
-                p = [x.strip() for x in p.split(',')]
-                people_and_position.append(p)
-
-
-
-            for ppp in people_and_position:
-                people_dict[symbol][ppp[0]] = ppp[1]
-
-
-pp.pprint(people_dict)
 
 
 
