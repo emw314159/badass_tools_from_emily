fe4f907c9978a2a7d6da9d31ed96be4725c63521
Emily Williams <emw314159@gmail.com>
2017-07-26 13:58:04 -0700

    Parallelized it.

--- stock_analysis/granger_causality.py
+++ stock_analysis/granger_causality.py
@@ -9,6 +9,7 @@ import pprint as pp
 import glob
 import json
 import pandas as pd
+from multiprocessing import Process, Pool
 
 #
 # user settings
@@ -17,8 +18,8 @@ output_directory = 'output'
 quote_data_directory = 'quote_data'
 threshold = 0.05
 number_of_lags = 5
-###run_self_volume_to_adj_close = False
-run_NxN_volume_to_adj_close = True
+number_of_workers_in_pool = 20
+chunksize = 10
 
 #
 # get symbol list
@@ -26,107 +27,100 @@ run_NxN_volume_to_adj_close = True
 symbol_list = sorted([x.split('/')[1].replace('.pickle', '') for x in glob.glob(quote_data_directory + '/*.pickle')])
 
 #
-# function to load and prepare stock data
+# wrapper for parallelization
 #
-def load_and_prepare_stock_data(symbol):
-    with open(quote_data_directory + '/' + symbol + '.pickle') as f:
-        df = pickle.load(f)
-    percent_diff = [NaN]
-    percent_diff.extend( [((j - i) / i) * 100. for i, j in zip(df['Adj Close'][0:-1], df['Adj Close'][1:])] )
-    df['Percent Difference Adj Close'] = percent_diff
-    return df
+def wrapper(ij):
+
+    #
+    # function to load and prepare stock data
+    #
+    def load_and_prepare_stock_data(symbol):
+        with open(quote_data_directory + '/' + symbol + '.pickle') as f:
+            df = pickle.load(f)
+        percent_diff = [NaN]
+        percent_diff.extend( [((j - i) / i) * 100. for i, j in zip(df['Adj Close'][0:-1], df['Adj Close'][1:])] )
+        df['Percent Difference Adj Close'] = percent_diff
+        return df
+
+    #
+    # calculate causality
+    #
+    def calculate_causality(array_to_test):
+        causality = grangercausalitytests(array_to_test, number_of_lags, verbose=False)
+
+        lag_dict = {}
+        for lag in sorted(causality.keys()):
+            tests = causality[lag][0]
+            p_list = []
+            for t in tests.keys():
+                p = tests[t][1]
+                p_list.append(p)
+
+            mean_p_list = mean(p_list)
+            if mean_p_list <= threshold:
+                log_mean_p_list = log(mean_p_list, 10.)
+                log_mean_p_list_str = '%0.2f' % log_mean_p_list
+                lag_dict[lag] = float(log_mean_p_list_str)
+        return lag_dict
 
 
-#
-# calculate causality
-#
-def calculate_causality(array_to_test):
-    causality = grangercausalitytests(array_to_test, number_of_lags, verbose=False)
+    i, j = ij
+    results = {}
+    try:
+        df_i = load_and_prepare_stock_data(i)
+        df_j = load_and_prepare_stock_data(j)
+        df = pd.merge(df_i, df_j, left_index=True, right_index=True)
 
-    lag_dict = {}
-    for lag in sorted(causality.keys()):
-        tests = causality[lag][0]
-        p_list = []
-        for t in tests.keys():
-            p = tests[t][1]
-            p_list.append(p)
+        for col in [
+            'Open_x', 'High_x', 'Low_x', 'Close_x', 'Adj Close_x', 'Volume_x',
+            'Open_y', 'Close_y', 'High_y', 'Low_y', 'Adj Close_y', 'Percent Difference Adj Close_y']:
+            del(df[col])
 
-        mean_p_list = mean(p_list)
-        if mean_p_list <= threshold:
-            log_mean_p_list = log(mean_p_list, 10.)
-            log_mean_p_list_str = '%0.2f' % log_mean_p_list
-            lag_dict[lag] = float(log_mean_p_list_str)
-    return lag_dict
+        array_to_test = df.ix[1:, :].as_matrix()
 
-# #
-# # selfwise comparisons
-# #
-# self_causality = {}
-# if run_self_volume_to_adj_close:
-#     for symbol in symbol_list:
-#         df = load_and_prepare_stock_data(symbol)
-#         array_to_test = df.ix[1:, ['Percent Difference Adj Close', 'Volume']].as_matrix()
-#         try:
-#             results = calculate_causality(array_to_test)
-#             if results != {}:
-#                 self_causality[symbol] = results
-#         except:
-#             continue
+        results = calculate_causality(array_to_test)
 
-#     #
-#     # save self_causality
-#     #
-#     with open(output_directory + '/self_causality.json', 'w') as f:
-#         json.dump(self_causality, f)
-
-
-
+    except:
+        pass
+    return (i, j, results)
+    
 
 #
 # pairwise comparisons
 #
+pool = Pool(processes=number_of_workers_in_pool)
+
+arg_list = []
+for i in symbol_list:
+    print i
+    for j in symbol_list:
+        arg_list.append((i, j,))
+    break
+
 pairwise_causality = {}
+result = []
 
-#break_all = False
+it = pool.imap_unordered(wrapper, arg_list, chunksize=chunksize)
 
-if run_NxN_volume_to_adj_close:
-    for i in symbol_list:
+current = it.next()
+while current:
+    i, j, results = current
+    if results != {}:
+        if not pairwise_causality.has_key(i):
+            pairwise_causality[i] = {}
+        pairwise_causality[i][j] = results
+    try:
+        current = it.next()
+    except:
+        break
 
-        print i
+#
+# save pairwise causalitity
+#
+with open(output_directory + '/pairwise_causality.json', 'w') as f:
+    json.dump(pairwise_causality, f)
 
-        for j in symbol_list:
 
-            df_i = load_and_prepare_stock_data(i)
-            df_j = load_and_prepare_stock_data(j)
 
-            df = pd.merge(df_i, df_j, left_index=True, right_index=True)
-
-            for col in [
-                'Open_x', 'High_x', 'Low_x', 'Close_x', 'Adj Close_x', 'Volume_x',
-                'Open_y', 'Close_y', 'High_y', 'Low_y', 'Adj Close_y', 'Percent Difference Adj Close_y']:
-                del(df[col])
-
-            array_to_test = df.ix[1:, :].as_matrix()
-
-            try:
-                results = calculate_causality(array_to_test)
-                if results != {}:
-
-                    #break_all = True
-
-                    if not pairwise_causality.has_key(i):
-                        pairwise_causality[i] = {}
-                    pairwise_causality[i][j] = results
-            except:
-                continue
-
-        #if break_all:
-        #    break
-
-    #
-    # save pairwise causalitity
-    #
-    with open(output_directory + '/pairwise_causality.json', 'w') as f:
-        json.dump(pairwise_causality, f)
 
 
