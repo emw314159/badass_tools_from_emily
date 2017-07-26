7d0418b5b2905b45b2391b03a481e1f774d57364
Emily Williams <emw314159@gmail.com>
2017-07-26 14:04:48 -0700

    Added a procedure to not repeat failed attempts during restarts.

--- stock_analysis/get_quotes.py
+++ stock_analysis/get_quotes.py
@@ -5,6 +5,7 @@ import pandas_datareader as pdr
 from datetime import datetime
 import pickle
 import json
+import glob
 
 #
 # user settings
@@ -19,17 +20,42 @@ with open(output_directory + '/symbol_to_name.json') as f:
     symbol_to_name = json.load(f)
 symbol_list = sorted(symbol_to_name.keys())
 
+
+#
+# figure out what we have already
+#
+file_list = glob.glob(quote_data_directory + '/*.pickle')
+already_have = [x.split('/')[1].replace('.pickle', '') for x in file_list]
+
+#
+# figure out what we've tried already that didn't work.
+#
+with open(output_directory + '/tried_retrieving_unsuccessfully.json') as f:
+    tried_getting = json.load(f)
+
+
 #
 # iterate through the symbols
 #
 for symbol in symbol_list:
+
+    if symbol in already_have:
+        continue
+    if symbol in tried_getting:
+        continue
+    
+
     print symbol
-    df = pdr.get_data_yahoo(symbols=symbol, start=datetime(2000, 1, 1), end=datetime(2017, 7, 25))
-    with open(quote_data_directory + '/' + symbol + '.pickle', 'w') as f:
-        pickle.dump(df, f)
 
-
-
+    try:
+        df = pdr.get_data_yahoo(symbols=symbol, start=datetime(2000, 1, 1), end=datetime(2017, 7, 25))
+        with open(quote_data_directory + '/' + symbol + '.pickle', 'w') as f:
+            pickle.dump(df, f)
+    except:
+        tried_getting.append(symbol)
+        with open(output_directory + '/tried_retrieving_unsuccessfully.json', 'w') as f:
+            json.dump(tried_getting, f)
+        continue
 
 
 
