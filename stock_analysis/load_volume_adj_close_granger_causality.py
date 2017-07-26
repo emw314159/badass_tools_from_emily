617d5f42197988eafaa2744943b58698b6b455d4
Emily Williams <emw314159@gmail.com>
2017-07-26 14:30:03 -0700

    Cypher commands to add pairwise Granger causality data.

---
+++ stock_analysis/load_volume_adj_close_granger_causality.py
@@ -0,0 +1,40 @@
+
+#
+# import useful libraries
+#
+import json
+import pprint as pp
+
+#
+# user settings
+#
+output_directory = 'output'
+
+#
+# load data
+#
+with open(output_directory + '/pairwise_causality.json') as f:
+    pairwise = json.load(f)
+
+#
+# start cypher file and eliminate relationships
+#
+f = open('output/cypher_commands_granger_volume_adj_close.txt', 'w')
+f.write('MATCH ()-[r:VOLUME_GRANGER_CAUSES_ADJ_CLOSE]-() DELETE r;' + '\n')
+
+#
+# build cypher commands
+#
+for close in sorted(pairwise.keys()):
+    for volume in sorted(pairwise[close].keys()):
+        for lag in sorted(pairwise[close][volume].keys()):
+            lag = int(lag)
+            p_log_10 = pairwise[close][volume][str(lag)]
+            cmd = 'MATCH (volume:COMPANY {id : \'' + volume + '\'}), (close:COMPANY {id : \'' + close + '\'}) CREATE UNIQUE (volume)-[r:VOLUME_GRANGER_CAUSES_ADJ_CLOSE]-(close) SET r.lag = ' + str(lag) + ' AND r.p_log_10 = ' + str(p_log_10) + ' RETURN volume, close, r;'
+            f.write(cmd + '\n')
+
+#
+# close cypher file
+#
+f.close()
+
