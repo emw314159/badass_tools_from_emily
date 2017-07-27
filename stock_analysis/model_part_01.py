a55ad2b3a97c3c6f9eb21712b8d661b7d6e6726e
Emily Williams <emw314159@gmail.com>
2017-07-26 23:53:04 -0700

    Stopping for the night. Need to load database first anyway.

--- stock_analysis/model_part_01.py
+++ stock_analysis/model_part_01.py
@@ -7,6 +7,7 @@ import pprint as pp
 from numpy import NaN, percentile
 import matplotlib.pyplot as plt
 from datetime import timedelta
+from neo4j.v1 import GraphDatabase, basic_auth
 
 #
 # user settings
@@ -14,7 +15,22 @@ from datetime import timedelta
 output_directory = 'output'
 quote_data_directory = 'quote_data'
 volume_threshold = 1000000
-calculate_events = True
+database_lags = 2
+calculate_events = False
+calculate_database = False
+
+user = 'neo4j'
+password = 'aoeuI111'
+
+weekday_map = {
+    0 : 'M',
+    1 : 'Tu',
+    2 : 'W',
+    3 : 'Th',
+    4 : 'F',
+    5 : 'Sa',
+    6 : 'Su',
+}
 
 #
 # function to load and prepare stock data
@@ -42,15 +58,17 @@ dt_52_week = timedelta(days=-365)
 dt_4_week = timedelta(days=-28)
 dt_12_week = timedelta(days=-84)
 dt_1_day = timedelta(days=-1)
+symbols_with_event = {}
 
 if calculate_events:
     events = []
-    for symbol in symbol_list:
+    for symbol in symbol_list[0:15]:
         df = load_and_prepare_stock_data(symbol)
         start_date = df.index[0]
         end_date = df.index[-1]
         idx = df.ix[df['Percent Difference Volume'] >= volume_threshold, :].index
         for i in idx:
+            symbols_with_event[symbol] = None
             if i == end_date:  continue
             if i + dt_52_week < start_date:  continue
 
@@ -69,7 +87,7 @@ if calculate_events:
             events.append({
                     'mover_percent_diff_volume' : df.ix[i,:]['Percent Difference Volume'],
                     'mover_symbol' : symbol,
-                    'weekday' : weekday,
+                    'weekday' : weekday_map[weekday],
                     'mover_percent_52_week_high' : percent_52_week_high,
                     'mover_percent_12_week_high' : percent_12_week_high,
                     'mover_percent_4_week_high' : percent_4_week_high,
@@ -83,11 +101,28 @@ if calculate_events:
 
     with open(output_directory + '/events.pickle', 'w') as f:
         pickle.dump(events, f)
+    with open(output_directory + '/symbols_with_events.pickle', 'w') as f:
+        pickle.dump(symbols_with_events, f)
 
 else:
     with open(output_directory + '/events.pickle') as f:
         events = pickle.load(f)
+    with open(output_directory + '/symbols_with_events.pickle') as f:
+        symbols_with_event = pickle.load(f)
 
+#
+# get content from database
+#
+driver = GraphDatabase.driver('bolt://localhost:7687', auth=basic_auth(user, password))
+session = driver.session()
+
+if calculate_database:
+    for symbol in symbols_with_event[0:15]:
+        cmd = 'MATCH (volume:COMPANY)-[r:VOLUME_GRANGER_CAUSES_ADJ_CLOSE]->(close:COMPANY) WHERE r.lag = ' + str(database_lag) + ' AND volume.id = ' + symbol + ' RETURN volume.id AS volume, close.id AS close, r.p_log_10 AS p_log_10;'
+        result = session.run(cmd)
+
+        print
+        print result
 
 
 
