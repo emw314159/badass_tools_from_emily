a50b66c6c0892076fa9e16d805a2d1c824e5997e
Emily Williams <emw314159@gmail.com>
2017-08-01 08:00:27 -0700

    Added substantial comments. Cleaned up code. Improved formula.

--- stock_analysis/model_02/02.py
+++ stock_analysis/model_02/02.py
@@ -1,20 +1,18 @@
 
 #
-# load useful libraries
+# load useful standard libraries
 #
 import pprint as pp
 import pandas as pd
-
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
 from numpy import percentile, isnan, NaN
 import sys
 import random
 import json
 import pickle
 
+#
+# load my libraries
+#
 import badass_tools_from_emily.machine_learning.machine_learning as ml
 import badass_tools_from_emily.machine_learning.random_forest as rf
 
@@ -23,39 +21,34 @@ import badass_tools_from_emily.machine_learning.random_forest as rf
 #
 random.seed(234)
 output_directory = 'output'
+plot_directory = '/Users/emily/Desktop/stocks'
 bad_cutoff_percentile = 40.
 good_cutoff_percentile = 85.
 number_of_vfolds_to_run = 100
-cost = 8192.
-gamma = 0.125
+number_of_random_forest_jobs = 2000
 lead_variable = 'percent_diff_lead_1_to_lead_2'
 file_to_load = 'output/TEMP_data_for_modeling.csv'
-#file_to_load = 'output/FREEZE.csv'
+compute_rf = True
 
-compute_rf = False
+#formula = 'y ~ lag_0 + lag_1 + lag_2 + lag_3 + lag_4 + lag_5 + len_features_list + mean_median_diff + p_0 + p_100 + p_25 + p_50 + p_75 + percent_high_month + percent_high_quarter + percent_high_year + C(weekday)'
 
-
-libsvm_root = '/Users/emily/Desktop/packages/libsvm-3.22'
-full_model_file = 'output/FULL_SVM'
+formula = 'y ~ lag_0 + lag_1 + lag_2 + lag_3 + lag_4 + lag_5 + p_50 + percent_high_month + p_0 + p_100 + len_features_list + mean_median_diff'
 
 
-formula = 'y ~ lag_0 + lag_1 + lag_2 + lag_3 + lag_4 + lag_5 + len_features_list + mean_median_diff + p_0 + p_100 + p_25 + p_50 + p_75 + percent_high_month + percent_high_quarter + percent_high_year + C(weekday)'
-
-#formula = 'y ~ lag_0 + lag_1 + lag_2 + lag_3 + lag_4 + lag_5 + p_50 + percent_high_year'
-
-formula = 'y ~ lag_0 + lag_1 + lag_2 + lag_3 + lag_4 + lag_5 + p_50 + percent_high_month + p_0 + p_100'
 
 
 factor_options = {
     'weekday' : ['M', 'Tu', 'W', 'Th', 'F']
     }
 
-
 #
 # load data
 #
 df = pd.read_csv(file_to_load)
 
+#
+# get rid of any NaNs
+#
 idx_list = []
 for i, idx in enumerate(df.index):
     fine = True
@@ -82,10 +75,8 @@ bad_cutoff = percentile(df[lead_variable], bad_cutoff_percentile)
 good_cutoff = percentile(df[lead_variable], good_cutoff_percentile)
 print
 
-
-
 #
-# apply cutoffs
+# apply cutoffs given computed percentiles
 #
 df_to_use = df.ix[(df[lead_variable] <= bad_cutoff) | (df[lead_variable] >= good_cutoff), :].copy()
 y = []
@@ -101,70 +92,45 @@ for x in df_to_use[lead_variable]:
         sys.exit(0)
 df_to_use['y'] = y
 
-
-
 #
 # set up model
 #
 y, X = ml.categorize(formula, factor_options, df_to_use)
-#y, X = ml.categorize(formula, factor_options, df_to_use, add_intercept=False)
-
-
-
-
 
 #
-# random forest
+# figure out which features matter the most using random forest classification
 #
 if compute_rf:
     yrf, Xrf = ml.categorize(formula, factor_options, df_to_use, add_intercept=False)
-    model = ml.random_forest_classification_wrapper(yrf, Xrf, n_jobs=2000)
+    model = ml.random_forest_classification_wrapper(yrf, Xrf, n_jobs=number_of_random_forest_jobs)
     feature_importances = model.model.feature_importances_
     fi_dict = {}
     for h, fi in zip(list(Xrf.columns.values), feature_importances):
         fi_dict[h] = fi
-    rf.plot_feature_importances(fi_dict, 'Relative Feature Importances', '/Users/emily/Desktop/stocks/feature_importances.png')
-
-
-
-#
-# get full model for grid.py
-#
-#model = ml.svm_wrapper(y, X, c=cost, g=gamma, output_file=full_model_file, libsvm_root=libsvm_root)
-#with open(full_model_file + '.pickle', 'w') as f:
-#    pickle.dump(model, f)
-
-#sys.exit(0)
-
-#python ~/Desktop/packages/libsvm-3.22/tools/grid.py -svmtrain ~/Desktop//packages/libsvm-3.22/svm-train -gnuplot "null" -out output/grid.out -b 1 output/FULL_SVM.scaled
-
+    rf.plot_feature_importances(fi_dict, 'Relative Feature Importances', plot_directory + '/feature_importances.png')
 
 #
 # get full model for logistic regression
 #
 model = ml.logit_wrapper(y, X)
-with open('output/logit_model.pickle', 'w') as f:
+with open(output_directory + '/logit_model.pickle', 'w') as f:
     pickle.dump(model, f)
-
-
 
 #
 # cross-validate
 #
-#results = ml.v_fold(ml.svm_wrapper, y, X, number_of_vfolds_to_run, c=cost, g=gamma, output_file='output/SVM_CV', libsvm_root=libsvm_root)
 results = ml.v_fold(ml.logit_wrapper, y, X, number_of_vfolds_to_run)
 
-pp.pprint(results['auc_list'])
-
-
-ml.plot_auc_histogram(results, 'Cross-Validation AUC Histogram', '/Users/emily/Desktop/stocks/HIST_AUC.png', color='lightblue')
-
-
-
-fpr, tpr, roc_auc, thresholds = ml.plot_a_representative_ROC_plot(results, 'ROC Curve for Stock Prediction', '/Users/emily/Desktop/stocks/ROC.png')
 print
 pp.pprint(results['auc_list'])
 print
+
+#
+# plot histogram and ROC plots for the cross-validation effort
+#
+ml.plot_auc_histogram(results, 'Cross-Validation AUC Histogram (n = ' + str(number_of_vfolds_to_run) + ')', plot_directory + '/HIST_AUC.png', color='lightblue')
+
+fpr, tpr, roc_auc, thresholds = ml.plot_a_representative_ROC_plot(results, 'ROC Curve for Stock Prediction', plot_directory + '/ROC.png')
 
 #
 # save fpr, tpr, roc_auc, thresholds
@@ -175,8 +141,8 @@ to_save = {
     'roc_auc' : roc_auc,
     'thresholds' : thresholds,
 }
-#with open(output_directory + '/fpr_tpr_thresholds.pickle', 'w') as f:
-#    pickle.dump(to_save, f)
+with open(output_directory + '/fpr_tpr_thresholds.pickle', 'w') as f:
+    pickle.dump(to_save, f)
 
 
 #
