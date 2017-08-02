bedee0ed5dfc25971e9d80fcb74eee024818dac4
EC2 Default User <ec2-user@ip-172-31-10-74.us-west-2.compute.internal>
2017-08-02 02:34:09 +0000

    Forced the package path in the header.

--- stock_analysis/model_02/daily/pipeline.py
+++ stock_analysis/model_02/daily/pipeline.py
@@ -1,4 +1,12 @@
 
+# python badass_tools_from_emily/stock_analysis/model_02/daily/pipeline.py badass_tools_from_emily/stock_analysis/model_02/daily/config.json 
+
+
+
+import sys
+sys.path.insert(0, '/home/ec2-user')
+
+
 import badass_tools_from_emily.stock_analysis.model_02.daily.initialize as initialize
 import badass_tools_from_emily.stock_analysis.model_02.daily.query_database_for_movers as query_database_for_movers
 import badass_tools_from_emily.stock_analysis.model_02.daily.get_two_day_stocks as get_two_day_stocks
