6036b7a1e8542aefd578c779b868e51d9e11492b
EC2 Default User <ec2-user@ip-172-31-10-74.us-west-2.compute.internal>
2017-08-02 02:05:22 +0000

    Functions now.

--- stock_analysis/model_02/daily/initialize.py
+++ stock_analysis/model_02/daily/initialize.py
@@ -1,31 +1,34 @@
-#
-# load useful libraries
-#
-import json
-import datetime
-import pickle
-import sys
-import os
 
-#
-# load configuration
-#
-with open(sys.argv[1]) as f:
-    config = json.load(f)
+def initialize():
 
-#
-# user settings
-#
-runtime_output_directory = config['runtime_output_directory']
+    #
+    # load useful libraries
+    #
+    import json
+    import datetime
+    import pickle
+    import sys
+    import os
 
-# remove runtime output directory
-os.system('rm -R ' + runtime_output_directory)
-os.system('mkdir ' + runtime_output_directory)
+    #
+    # load configuration
+    #
+    with open(sys.argv[1]) as f:
+        config = json.load(f)
 
-#
-# establish yesterday's date
-#
-end = (datetime.datetime.now() + datetime.timedelta(days=-1)).date()
-with open(runtime_output_directory + '/end.pickle', 'w') as f:
-    pickle.dump(end, f)
+    #
+    # user settings
+    #
+    runtime_output_directory = config['runtime_output_directory']
+
+    # remove runtime output directory
+    os.system('rm -R ' + runtime_output_directory)
+    os.system('mkdir ' + runtime_output_directory)
+
+    #
+    # establish yesterday's date
+    #
+    end = (datetime.datetime.now() + datetime.timedelta(days=-1)).date()
+    with open(runtime_output_directory + '/end.pickle', 'w') as f:
+        pickle.dump(end, f)
 
--- stock_analysis/model_02/daily/query_database_for_movers.py
+++ stock_analysis/model_02/daily/query_database_for_movers.py
@@ -1,57 +1,59 @@
-#
-# load useful libraries
-#
-import pickle
-import datetime
-from neo4j.v1 import GraphDatabase, basic_auth
-import json
-import sys
+def query_database_for_movers():
 
-#
-# load configuration
-#
-with open(sys.argv[1]) as f:
-    config = json.load(f)
+    #
+    # load useful libraries
+    #
+    import pickle
+    import datetime
+    from neo4j.v1 import GraphDatabase, basic_auth
+    import json
+    import sys
 
-#
-# user settings
-#
-user = config['user']
-password = config['password']
-database_lags = config['database_lags']
-runtime_output_directory = config['runtime_output_directory']
+    #
+    # load configuration
+    #
+    with open(sys.argv[1]) as f:
+        config = json.load(f)
 
-#
-# load end date
-#
-with open(runtime_output_directory + '/end.pickle') as f:
-    end = pickle.load(f)
+    #
+    # user settings
+    #
+    user = config['user']
+    password = config['password']
+    database_lags = config['database_lags']
+    runtime_output_directory = config['runtime_output_directory']
 
-#
-# connect to database
-#
-driver = GraphDatabase.driver('bolt://localhost:7687', auth=basic_auth(user, password))
-session = driver.session()
+    #
+    # load end date
+    #
+    with open(runtime_output_directory + '/end.pickle') as f:
+        end = pickle.load(f)
 
-#
-# get volume movers known to database
-#
-cmd = 'MATCH (volume:COMPANY)-[r:VOLUME_GRANGER_CAUSES_ADJ_CLOSE]->(close:COMPANY) WHERE r.lag = ' + str(database_lags) + ' RETURN volume.id AS volume, close.id AS close, r.lag as lag, r.p_log_10 as p_log_10;'
-volume_movers = {}
-result = session.run(cmd)
-for record in result:
-    volume = record['volume']
-    close = record['close']
-    lag = str(record['lag'])
-    p_log_10 = record['p_log_10']
+    #
+    # connect to database
+    #
+    driver = GraphDatabase.driver('bolt://localhost:7687', auth=basic_auth(user, password))
+    session = driver.session()
 
-    if not volume_movers.has_key(volume):
-        volume_movers[volume] = {}
-    if not volume_movers[volume].has_key(close):
-        volume_movers[volume][close] = p_log_10
+    #
+    # get volume movers known to database
+    #
+    cmd = 'MATCH (volume:COMPANY)-[r:VOLUME_GRANGER_CAUSES_ADJ_CLOSE]->(close:COMPANY) WHERE r.lag = ' + str(database_lags) + ' RETURN volume.id AS volume, close.id AS close, r.lag as lag, r.p_log_10 as p_log_10;'
+    volume_movers = {}
+    result = session.run(cmd)
+    for record in result:
+        volume = record['volume']
+        close = record['close']
+        lag = str(record['lag'])
+        p_log_10 = record['p_log_10']
 
-with open(runtime_output_directory + '/volume_movers.json', 'w') as f:
-    json.dump(volume_movers, f)
+        if not volume_movers.has_key(volume):
+            volume_movers[volume] = {}
+        if not volume_movers[volume].has_key(close):
+            volume_movers[volume][close] = p_log_10
+
+    with open(runtime_output_directory + '/volume_movers.json', 'w') as f:
+        json.dump(volume_movers, f)
 
 
 
