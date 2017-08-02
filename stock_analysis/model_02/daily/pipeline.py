12cd0d8ac3f25963106b4c9a89c6d4c034a25b3f
EC2 Default User <ec2-user@ip-172-31-10-74.us-west-2.compute.internal>
2017-08-02 02:18:23 +0000

    Initial import.

---
+++ stock_analysis/model_02/daily/pipeline.py
@@ -0,0 +1,13 @@
+
+import badass_tools_from_emily.stock_analysis.model_02.daily.initialize as initialize
+import badass_tools_from_emily.stock_analysis.model_02.daily.query_database_for_movers as query_database_for_movers
+import badass_tools_from_emily.stock_analysis.model_02.daily.get_two_day_stocks as get_two_day_stocks
+import badass_tools_from_emily.stock_analysis.model_02.daily.get_year_stocks as get_year_stocks
+import badass_tools_from_emily.stock_analysis.model_02.daily.prepare_for_prediction as prepare_for_prediction
+
+initialize.initialize()
+query_database_for_movers.query_database_for_movers()
+get_two_day_stocks.get_two_day_stocks()
+get_year_stocks.get_year_stocks()
+prepare_for_prediction.prepare_for_prediction()
+
