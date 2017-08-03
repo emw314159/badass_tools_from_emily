2bfa4dc2a92ffb8bbbbb656eb38f633fef8949a0
EC2 Default User <ec2-user@ip-172-31-10-74.us-west-2.compute.internal>
2017-08-03 20:04:41 +0000

    Initial import.

---
+++ web_apps/stocks/phoenix/static/phoenix_model_info.js
@@ -0,0 +1,28 @@
+var app = angular.module('phoenix_model_info_app', []);
+
+
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.headers.common["X-CSRFToken"] = window.csrf_token;
+}]);
+
+app.controller('phoenix_model_info_controller', ['$scope', '$http', function($scope, $http) {
+        
+    /*
+      Initialize variables
+    */
+    $scope.data = [];
+    var url = 'http://ec2-35-160-25-103.us-west-2.compute.amazonaws.com:8080/phoenix/modelreport'
+
+    /*
+      call for data
+     */
+    $http({
+        method : 'POST',
+        data : {},
+        url : url,
+    }).then(function successCallback(response) {
+	$scope.data = response['data']['data'];
+    }, function errorCallback(response) {
+    });
+}]);
