545d116096a9c234779d46e84db8265ec26eebcc
Emily Williams <emw314159@gmail.com>
2017-08-07 18:14:58 -0700

    Progress...

--- stock_analysis/harmonic/harmonic_01.py
+++ stock_analysis/harmonic/harmonic_01.py
@@ -44,9 +44,7 @@ def which_list(idx, first, second):
 #
 def stage_1(args):
 
-    t, ts_narrow, max_index_range, scan_end_point_only = args
-
-
+    t, ts_narrow, max_index_range, scan_end_point_only, first, second = args
 
     # check max index range
     if max(t) - min(t) > max_index_range:
@@ -61,11 +59,21 @@ def stage_1(args):
     values = [ts_narrow[i] for i in t]
     diff = [int(np.sign(y - x)) for x, y in zip(values[0:-1], values[1:])]
     diff_test = [y != x for x, y in zip(diff[0:-1], diff[1:])]
-
-    if not False in diff_test:
-        return True, t
-    else:
+    if False in diff_test:
         return False, t
+
+    # ensure sorted
+    if False in [y > x for x, y in zip(t[0:-1], t[1:])]:
+        return False, t
+    
+    # ensure oscillation
+    test_list = [which_list(i, first, second) for i in t]
+    test = [y != x for x, y in zip(test_list[0:-1], test_list[1:])]
+    if False in test:
+        return False, t
+
+    # everything checks out
+    return True, t
 
 #
 # main
@@ -83,7 +91,7 @@ if __name__ == "__main__":
     plot_directory = '/Users/emily/Desktop/stocks'
     order = 10
     scan_end_point_only = False
-    ratio_of_CD_to_look_ahead = 1.0
+    ratio_of_CD_to_look_ahead = [0.5, 0.75, 1.0, 1.25, 1.5]
     max_index_range = int(round(365. / 2.))
     number_of_workers_in_pool = 20
     chunksize = 1000
@@ -103,8 +111,8 @@ if __name__ == "__main__":
     # narrow down window
     #
     range_finder = [x >= start and x <= end for x in ts.index]
-    #ts_narrow = ts.ix[range_finder].copy()
-    ts_narrow = ts
+    ts_narrow = ts.ix[range_finder].copy()
+    #ts_narrow = ts
 
     #
     # find relative extrema
@@ -162,7 +170,7 @@ if __name__ == "__main__":
     a = datetime.datetime.now()
     data_list = []
     for t in test:
-        data = (t, ts_narrow, max_index_range, scan_end_point_only)
+        data = (t, ts_narrow, max_index_range, scan_end_point_only, first, second)
         data_list.append(data)
 
     #it = pool.apply_async(stage_1, data)
@@ -185,26 +193,26 @@ if __name__ == "__main__":
     b = datetime.datetime.now()
     print b - a 
 
-    print 'Stage 2...'
-    a = datetime.datetime.now()
-    permutations_stage_2 = []
-    for p in permutations:
-        if not False in [y > x for x, y in zip(p[0:-1], p[1:])]:
-            permutations_stage_2.append(p)
-    b = datetime.datetime.now()
-    print b - a 
+#     print 'Stage 2...'
+#     a = datetime.datetime.now()
+#     permutations_stage_2 = []
+#     for p in permutations:
+#         if not False in [y > x for x, y in zip(p[0:-1], p[1:])]:
+#             permutations_stage_2.append(p)
+#     b = datetime.datetime.now()
+#     print b - a 
 
-    print 'Stage 3...'
-    a = datetime.datetime.now()
-    permutations_stage_3 = []
-    for p in permutations_stage_2:
-        test_list = [which_list(i, first, second) for i in p]
-        test = [y != x for x, y in zip(test_list[0:-1], test_list[1:])]
-        if not False in test:
-            permutations_stage_3.append(p)
+#     print 'Stage 3...'
+#     a = datetime.datetime.now()
+#     permutations_stage_3 = []
+#     for p in permutations_stage_2:
+#         test_list = [which_list(i, first, second) for i in p]
+#         test = [y != x for x, y in zip(test_list[0:-1], test_list[1:])]
+#         if not False in test:
+#             permutations_stage_3.append(p)
 
-    b = datetime.datetime.now()
-    print b - a 
+#     b = datetime.datetime.now()
+#     print b - a 
 
 
 
@@ -212,9 +220,9 @@ if __name__ == "__main__":
     # compute ratios
     #
     a = datetime.datetime.now()
-    with_look_ahead = []
+    final = []
     last_index = len(ts_narrow) - 1
-    for p in permutations_stage_3:
+    for p in permutations:
         values = [ts_narrow[i] for i in p]
         diff = [y - x for x, y in zip(values[0:-1], values[1:])]
 
@@ -227,76 +235,76 @@ if __name__ == "__main__":
         time_diff_CD = p[4] - p[3]
         time_diff_XB = p[2] - p[0]
         time_diff_BD = p[4] - p[2]
+        time_diff_XD = p[4] - p[0]
         time_diff_ratio_XA_AB = float(time_diff_XA) / float(time_diff_AB)
         time_diff_ratio_BC_CD = float(time_diff_BC) / float(time_diff_CD)
         time_diff_ratio_XB_BD = float(time_diff_XB) / float(time_diff_BD)
 
-        look_ahead_idx = int(round(ratio_of_CD_to_look_ahead * time_diff_CD)) + p[-1]
-
         is_bull = int(np.sign(diff[0]) >= 1)
 
-        if look_ahead_idx <= last_index:
+        look_ahead_idx_list = [int(round(x * time_diff_CD)) + p[-1] for x in ratio_of_CD_to_look_ahead]
+        if look_ahead_idx_list[-1] <= last_index:
+
             end_price = ts_narrow[p[-1]]
-            look_ahead_price = ts_narrow[look_ahead_idx]
-            look_ahead_date = ts_narrow.index[look_ahead_idx]
-            percent_change = 100. * (look_ahead_price - end_price) / look_ahead_price
 
-            with_look_ahead.append({
-                    'is_bull' : is_bull,
-                    'p' : p,
-                    'look_ahead_price' : look_ahead_price,
-                    'look_ahead_date' : look_ahead_date,
-                    'percent_change' : percent_change,
-                    'ratio_CD_of_XA' : ratio_CD_of_XA,
-                    'ratio_AB_of_XA' : nearest_ratios[0],
-                    'ratio_BC_of_AB' : nearest_ratios[1],
-                    'ratio_CD_of_BC' : nearest_ratios[2],
-                    'time_diff_ratio_XA_AB' : time_diff_ratio_XA_AB,
-                    'time_diff_ratio_BC_CD' : time_diff_ratio_BC_CD,
-                    'time_diff_ratio_XB_BD' : time_diff_ratio_XB_BD,
-                    })
-    b = datetime.datetime.now()
-    print b - a 
+            with_look_ahead = {}
+            for lai, ratio_ahead in zip(look_ahead_idx_list, ratio_of_CD_to_look_ahead):
+                ratio_str = str(int(round(100. * ratio_ahead)))
+                with_look_ahead['look_ahead_price_' + ratio_str] = ts_narrow[lai]
+                with_look_ahead['look_ahead_date_' + ratio_str] = ts_narrow.index[lai]
+                with_look_ahead['percent_change_' + ratio_str] = 100. * (with_look_ahead['look_ahead_price_' + ratio_str] - end_price) / with_look_ahead['look_ahead_price_' + ratio_str]
+
+            with_look_ahead['is_bull'] = is_bull
+            with_look_ahead['p'] =  p
+            with_look_ahead['ratio_CD_of_XA'] = ratio_CD_of_XA
+            with_look_ahead['ratio_AB_of_XA'] = nearest_ratios[0]
+            with_look_ahead['ratio_BC_of_AB'] = nearest_ratios[1]
+            with_look_ahead['ratio_CD_of_BC'] = nearest_ratios[2]
+            with_look_ahead['time_diff_ratio_XA_AB'] = time_diff_ratio_XA_AB
+            with_look_ahead['time_diff_ratio_BC_CD'] = time_diff_ratio_BC_CD
+            with_look_ahead['time_diff_ratio_XB_BD'] = time_diff_ratio_XB_BD
+            with_look_ahead['time_diff_XD'] = time_diff_XD
+
+            final.append(with_look_ahead)
+
+#     #
+#     # plot time series
+#     #
+
+#     #p = random.sample(permutations, 1)[0]
+#     #P = permutations[-1]
+#     idx = -1
+#     p = with_look_ahead[idx]['p']
+#     look_ahead_price, look_ahead_date = with_look_ahead[idx]['look_ahead_price'], with_look_ahead[idx]['look_ahead_date']
+
+#     values = [ts_narrow.values[i] for i in p]
+#     dates = [ts_narrow.index[i] for i in p]
+#     pattern_values = [values[i] for i in [0, 2, 4, 2, 1, 3]]
+#     pattern_dates = [dates[i] for i in [0, 2, 4, 2, 1, 3]]
+
+#     plt.figure(figsize=[14, 10])
+#     plt.plot(ts_narrow.index, list(ts_narrow))
+#     plt.plot(pattern_dates, pattern_values, color='lightgray')
+#     plt.plot(dates, values, color='purple')
+#     plt.plot(peaks_and_valleys_dates, peaks_and_valleys_values, '.', color='green', ms=20)
+#     plt.plot(look_ahead_date, look_ahead_price, '.', color='red', ms=20)
+#     plt.title('Emily\'s Harmonic Scanner\n' + symbol + ' Share Price', fontsize=18)
+#     plt.xlabel('Date')
+#     plt.ylabel('Price (USD)')
+#     plt.savefig(plot_directory + '/ts_harmonic.png')
+#     plt.close()
 
 
     #
-    # plot time series
-    #
-
-    #p = random.sample(permutations_stage_3, 1)[0]
-    #P = permutations_stage_3[-1]
-    idx = -1
-    p = with_look_ahead[idx]['p']
-    look_ahead_price, look_ahead_date = with_look_ahead[idx]['look_ahead_price'], with_look_ahead[idx]['look_ahead_date']
-
-    values = [ts_narrow.values[i] for i in p]
-    dates = [ts_narrow.index[i] for i in p]
-    pattern_values = [values[i] for i in [0, 2, 4, 2, 1, 3]]
-    pattern_dates = [dates[i] for i in [0, 2, 4, 2, 1, 3]]
-
-    plt.figure(figsize=[14, 10])
-    plt.plot(ts_narrow.index, list(ts_narrow))
-    plt.plot(pattern_dates, pattern_values, color='lightgray')
-    plt.plot(dates, values, color='purple')
-    plt.plot(peaks_and_valleys_dates, peaks_and_valleys_values, '.', color='green', ms=20)
-    plt.plot(look_ahead_date, look_ahead_price, '.', color='red', ms=20)
-    plt.title('Emily\'s Harmonic Scanner\n' + symbol + ' Share Price', fontsize=18)
-    plt.xlabel('Date')
-    plt.ylabel('Price (USD)')
-    plt.savefig(plot_directory + '/ts_harmonic.png')
-    plt.close()
-
-
     #
     #
-    #
-    for la in with_look_ahead:
+    for la in final:
         del(la['p'])
-        del(la['look_ahead_date'])
-        del(la['look_ahead_price'])
+
+    pp.pprint(final)
 
 
 
 
-    df = pd.DataFrame(with_look_ahead)
+    df = pd.DataFrame(final)
     df.to_csv('data_for_regression.csv', index=False)
